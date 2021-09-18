bbc40c2b891dd557dea0afb4148b0b79d53fe3a4
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2021-09-17 22:33:47 -0400

    Changer la couleur du widget apt + le cacher si pas d'updates

--- .config/leftwm/themes/md-nord/polybar-scripts/updates-apt.sh
+++ .config/leftwm/themes/md-nord/polybar-scripts/updates-apt.sh
@@ -2,10 +2,8 @@
 
 updates=$(apt list --upgradable 2> /dev/null | grep -c upgradable);
 
-echo " $updates pkg "
-
-#if [ "$updates" -gt 0 ]; then
-    #echo "$updates pkg"
-#else
-    #echo ""
-#fi
+if [ "$updates" -gt 0 ]; then
+    echo "$updates pkg"
+else
+    echo ""
+fi
