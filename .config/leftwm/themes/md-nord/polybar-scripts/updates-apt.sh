4aa0cfe16d6dfbae277db3776f95f3334fe89d7b
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2021-10-18 21:28:23 -0400

    Changer la config de polybar et Leftwm

--- .config/leftwm/themes/md-nord/polybar-scripts/updates-apt.sh
+++ .config/leftwm/themes/md-nord/polybar-scripts/updates-apt.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-apt-get update 1> /dev/null
-updates=$(apt list --upgradable 2> /dev/null | grep -c upgradable);
+message=""
+sudo apt-get update 1> /dev/null
+updates=$(apt-get upgrade -s |grep -P '^\d+ upgraded'|cut -d" " -f1);
 
 if [ "$updates" -gt 0 ]; then
-    echo "$updates pkg"
-else
-    echo ""
+    message="$updates pkg"
 fi
+
+updates=0
+updates=$(apt-get upgrade -s |grep -P '^\d+ \w\ssecurity'|cut -d" " -f1);
+
+if [ ! -z "$updates" ] && [ "$updates" -gt 0 ]; then
+    message+=" $updates sec"
+fi
+
+echo $message
